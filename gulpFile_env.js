0f81a688386ab1524c5fdd87c178443a2ccc0f12
jackdizhu <376365334@qq.com>
2017-08-29 18:04:07 +0800

    .add 阳历日历数据生成

---
+++ gulpFile_env.js
@@ -0,0 +1,61 @@
+
+var gulp = require('gulp'),
+  fs = require('fs'),
+  less = require('gulp-less'),
+  csso = require('gulp-csso'),
+  livereload = require('gulp-livereload'),
+  uglify = require('gulp-uglify'),
+  minifycss = require('gulp-minify-css'),
+  rev = require('gulp-rev'),
+  replace = require('gulp-replace'),
+  revCollector = require('gulp-rev-collector'),
+  connect = require('gulp-connect');
+  rename = require('gulp-rename'),
+  sourcemaps = require('gulp-sourcemaps'),
+  notify=require('gulp-notify'),
+  plumber=require('gulp-plumber'),
+  nodemon=require('gulp-nodemon'),
+  express=require('gulp-express'),
+  clean = require('gulp-clean'),
+
+  gulp_webpack = require('gulp-webpack'),
+  webpack= require('webpack'),
+  webpack_config = require('./webpack.config_env.js'),
+
+  devHtml = require('gulp-devHtml');
+
+  var basePath = './';
+
+
+  //定义监听文件修改任务
+  gulp.task('watchBuild',(event) => {
+      // livereload.listen();
+      gulp.watch([basePath + 'src/index.js'], ['buildjs','minjs']);
+
+  });
+
+
+  // 生成js文件
+  gulp.task('buildjs',() => {
+    gulp.src(basePath + 'src/index.js')
+      .pipe(gulp_webpack(webpack_config,webpack))
+      .pipe(gulp.dest(basePath + 'dist/'))
+      // .pipe(devHtml({
+      //     files: ['./public/html/demo04.html']
+      // }))
+      // .pipe(livereload());
+  });
+
+  // 生成js文件
+  gulp.task('minjs',() => {
+    gulp.src(basePath + 'src/date/picker_date.js')
+      .pipe(uglify())
+      .pipe(gulp.dest(basePath + 'dist/'))
+  });
+
+  //定义默认任务
+  gulp.task('default',['watchBuild']);
+
+  gulp.task('min',['buildjs','minjs']);
+
+  gulp.run('min');
---
+++ src/h5_date.js
@@ -0,0 +1,125 @@
+// 依赖插件calendar
+var _lunar = {
+  // 阳历 转农历 等 详细信息
+  solar2lunar: calendar.solar2lunar
+};
+var H5_date = {
+  get_date: function (_obj) {
+    var _this = {
+      _yyyy: _obj._yyyy,
+      _mm: _obj._mm,
+      _dd: _obj._dd,
+      _el: _obj._el
+    }
+    var str = '';
+    str += this.add_prevM({
+      _yyyy: _this._yyyy,
+      _mm: _this._mm-1
+    });
+    str += this.add_thisM({
+      _yyyy: _this._yyyy,
+      _mm: _this._mm
+    });
+    str += this.add_nextM({
+      _yyyy: _this._yyyy,
+      _mm: _this._mm+1
+    });
+    if(_this._el){
+        document.getElementById(_this._el).innerHTML = str;
+    }
+    return str;
+  },
+  fn_maxD: function (_Y,_M) {
+      if (_M == '01' || _M == '03' || _M == '05' || _M == '07' || _M == '08' || _M == '10' || _M == '12') {
+          return 31;
+      }
+      else if (_M == '04' || _M == '06' || _M == '09' || _M == '11') {
+          return 30;
+      } else if (_M == '02' ){
+          var cond1 = _Y % 4 == 0;  //条件1：年份必须要能被4整除
+          var cond2 = _Y % 100 != 0;  //条件2：年份不能是整百数
+          var cond3 = _Y % 400 == 0;  //条件3：年份是400的倍数
+          //当条件1和条件2同时成立时，就肯定是闰年，所以条件1和条件2之间为“与”的关系。
+          //如果条件1和条件2不能同时成立，但如果条件3能成立，则仍然是闰年。所以条件3与前2项为“或”的关系。
+          //所以得出判断闰年的表达式：
+          var cond = cond1 && cond2 || cond3;
+          if (cond) {
+              return 29;
+          } else {
+              return 28;
+          }
+      }
+  },
+  // 获取上一月数据
+  add_prevM: function (_obj) {
+    // 获取上一月 最大天数
+    var _maxD = this.fn_maxD(_obj._yyyy,_obj._mm);
+    // 获取当前月 1号 信息
+    var _d = _lunar.solar2lunar(_obj._yyyy,_obj._mm+1,1);
+    var __n = _maxD - _d.nWeek;
+    var str = '',j;
+    if(_d.nWeek != 7){
+      for (var i = 0; i < _d.nWeek; i++) {
+        j = i + __n + 1;
+        if(i == 0){
+          str += '<tr>';
+        }
+        str += ' <td data_y="' + _obj._yyyy + '" data_m="' + _obj._mm + '" data_d="' + i + '" class="prevMonth"><i>' + j + '</i><em>' + '&nbsp;' + '</em></td>';
+      }
+    }else{
+      str += '<tr>';
+    }
+    return str;
+  },
+  // 获取当前一月数据
+  add_thisM: function (_obj) {
+    var _maxD = this.fn_maxD(_obj._yyyy,_obj._mm);
+    var _d = _lunar.solar2lunar(_obj._yyyy,_obj._mm,1);
+    var str = '',j;
+    var _wk = _d.nWeek;
+    var __wk = _d.nWeek;
+    for (var i = 0; i < _maxD; i++) {
+      j = i + 1;
+      // 当天星期几
+      __wk = _wk%7;
+      // if(_wk%7 == 0){
+      //  str += '<tr>';
+      // }
+      str += '<td data_y="' + _obj._yyyy + '" data_m="' + _obj._mm + '" data_d="' + i + '" class="thisMonth"><i>' + j + '</i><em>' + '&nbsp;' + '</em></td>';
+      if(__wk == 6){
+        str += '</tr>';
+      }
+      _wk++;
+    }
+    return str;
+  },
+  // 获取下一月数据
+  add_nextM: function (_obj) {
+    // 上一月 最大值
+    var _maxD = this.fn_maxD(_obj._yyyy,_obj._mm-1);
+    // 上一月 最后一天信息
+    var _d = _lunar.solar2lunar(_obj._yyyy,_obj._mm-1,_maxD);
+    var str = '',j;
+    var _wk = _d.nWeek;
+    // 如果上一月最后一天是 星期六
+    if(_wk == 6){
+      return '</tr>';
+    }
+    var __n = (14 - _wk - 1)%7;
+    var __wk = _d.nWeek;
+    for (var i = 0; i < __n; i++) {
+      j = i + 1;
+      // 当天星期几
+      __wk = _wk%7;
+      if(_wk%7 == 0){
+        str += '<tr>';
+      }
+      str += '<td data_y="' + _obj._yyyy + '" data_m="' + _obj._mm + '" data_d="' + i + '" class="nextMonth"><i>' + j + '</i><em>' + '&nbsp;' + '</em></td>';
+      if(__wk == 6 || i == _maxD){
+        str += '</tr>';
+      }
+      _wk++;
+    }
+    return str;
+  }
+};
